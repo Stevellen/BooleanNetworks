4ad38f10177bc34ea539a3cc3520551360c7c528
Steven Belcher <sbelcher@unomaha.edui>
2020-02-05 20:01:02 -0600

    Bifurcation Plot
    Added simple animated bifurcation plot

--- Chaos/Chaos1D.py
+++ Chaos/Chaos1D.py
@@ -6,6 +6,51 @@ University of Nebraska Omaha
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.animation import ArtistAnimation
+
+
+
+def bifurcation_plot(f=lambda x,a: a*x*(1-x), n_points=200, a_step=0.01, iterations=100, animate=True):
+    a_list = np.arange(1,4,a_step)
+    x_in = np.linspace(0,1,n_points)
+    x_out = np.empty((a_list.shape[0], ))
+    fig = plt.figure(figsize=(10,7))
+    artists = [[]]
+    for idx,a in enumerate(a_list):
+        x = np.copy(x_in)
+        for _ in range(iterations):
+            x = f(x,a)
+        im = plt.plot(np.repeat(a,x.shape[0]), x, color='blue', 
+                        marker='o', ls='', markersize=1)
+        artists.append(artists[idx]+im)
+    if animate:
+        anim = ArtistAnimation(fig, artists, interval=1, repeat=False)
+    plt.show()
+
+
+def plot_logistic_iteration(f, a, k):
+    x = np.linspace(0, 1, 300)
+    y = f(x, a)
+    square = np.sqrt(k)
+    # Ensure square is an integer with
+    if not square.is_integer():
+        square = int(square)+1
+    else:
+        square = int(square)
+
+    fig = plt.figure(figsize=(12, 7), dpi=100)
+    fig.suptitle(f'Generated {k} iterations for a = {a}')
+    for i in range(1, k+1):
+        ax = fig.add_subplot(square, square, i)
+        ax.plot(x, y)
+        ax.plot(x, x, 'r')
+        ax.grid(which='both', alpha=0.5)
+        y = f(y, a)
+    mngr = plt.get_current_fig_manager()
+    mngr.set_window_title('Iteration Plots')
+    # to put it into the upper left corner for example:
+    plt.show()
 
 
 def cobweb_plot(f, a, x0, k=40, show=True, save=False, filename='cobweb.png'):
@@ -70,5 +115,8 @@ if __name__ == "__main__":
     def f(x, a): return a*x*(1-x)
     x0 = np.random.rand()
     a = np.random.rand()*3+1
-    k = np.random.randint(0, 101)
+    k = 11
+    plot_logistic_iteration(f, a, k)
+    k = 100
     cobweb_plot(f, a, x0, k)
+    bifurcation_plot()
